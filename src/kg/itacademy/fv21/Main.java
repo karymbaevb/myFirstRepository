5d6bf3bd33a735725d1cce6fae2b3ab3f757a5bf
bkarymbaev <bkarymbaev@gmail.com>
2021-11-21 16:10:23 +0600

    Change greeting text

--- src/kg/itacademy/fv21/Main.java
+++ src/kg/itacademy/fv21/Main.java
@@ -3,6 +3,6 @@ package kg.itacademy.fv21;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello git");
+        System.out.println("Hello FV-21");
     }
 }
